0fcfe2c9b70e32660e289f22efef8f68abffc9dc
Bartek <bartlomiej.proczkowski@gmail.com>
2022-03-05 19:24:48 +0100

    Ready for deploy

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Title from "./components/Title";
 import UploadForm from "./components/UploadForm";
 import ImageGrid from "./components/ImageGrid";
 import Modal from "./components/Modal";
+import Footer from "./components/Footer";
 
 function App() {
   const [selectedImg, setSelectedImg] = useState(null);
@@ -15,6 +16,7 @@ function App() {
       {selectedImg && (
         <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
       )}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="footer">Created by B.Proczkowski @2021</div>;
+};
+
+export default Footer;
